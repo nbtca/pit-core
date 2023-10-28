18c9d01de5ee6ed299bf9a284932ee8ae5eda65d
LazuliKao <LazuliKao233@outlook.com>
2023-10-28 19:50:41 +0800

    sync

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"github.com/nbtca/pit-core/service"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 func main() {
-	router := mux.NewRouter()
-	go h.run()
-	router.HandleFunc("/ws", myws)
-	if err := http.ListenAndServe("0.0.0.0:8080", router); err != nil {
-		fmt.Println("err:", err)
+	//设置路由
+	http.HandleFunc("/", handler)
+	//监听端口
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//处理消息
+	go service.Loop(conn)
 }
---
+++ service/ws.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+struct User {
+	id int
+}
+
+var connections = make(map[*websocket.Conn]int)
+
+func processMessage(conn *websocket.Conn, msg string) {
+	conn.WriteMessage(websocket.TextMessage, []byte("hello"))
+}
+
+func open(conn *websocket.Conn) {
+	//添加连接
+	connections[conn] = 1
+
+}
+func close(conn *websocket.Conn) {
+	//删除连接
+	delete(connections, conn)
+}
+func Loop(conn *websocket.Conn) {
+	open(conn)
+	defer close(conn)
+	for {
+		messageType, data, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := conn.WriteMessage(messageType, data); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(data))
+		processMessage(conn, string(data))
+	}
+}
